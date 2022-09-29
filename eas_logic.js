172153a5483bdfd0e5d6b11fa824e62538cc47f9
DannyJonak <dannyjonak@gmail.com>
2022-09-28 20:55:58 -0400

    implement function to draw on grid, update buildGrid to take a sidelength parameter

--- eas_logic.js
+++ eas_logic.js
@@ -1,6 +1,8 @@
-function buildGrid() {
-    const sideLength = 16;
+function buildGrid(sideLength) {
+
     const grid = document.querySelector('.grid');
+    grid.setAttribute(`style', 'grid-template-rows = repeat(${sideLength}, 1fr);
+                        grid-template-columns = repeat(${sideLength}, 1fr)`);
 
     for (let i = 0; i < sideLength * sideLength; i++) {
         const newBox = document.createElement('div');
@@ -9,3 +11,25 @@ function buildGrid() {
     }
 }
 
+function setupHover() {
+    const gridSquares = document.querySelectorAll('.grid-box')
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('mousedown', (event) => {
+            square.style.backgroundColor = 'black';
+        })
+        square.addEventListener('mouseover', (event) => {
+            if (event.buttons === 1) {
+                event.preventDefault();
+                square.style.backgroundColor = 'black';
+            };
+        })
+    })
+}
+
+function changeGridSize() {
+
+}
+
+buildGrid();
+setupHover();
